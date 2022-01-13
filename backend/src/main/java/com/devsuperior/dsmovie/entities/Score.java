0346150b13025f662c268da92615c711d3cd3636
Leonardo Eduardo <leonardo.eduardo@gmail.com>
2022-01-13 08:20:46 -0300

    Domain model and Database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,46 @@
+package com.devsuperior.dsmovie.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embedded;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private ScorePk id;
+	
+	private Double value;
+	
+	public Score() {
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}	
+		
+	public void setUser(User user) {
+		id.setUser(user);	
+	}
+	
+	public ScorePk getId() {
+		return id;
+	}
+	public void setId(ScorePk id) {
+		this.id = id;
+	}
+	public Double getValue() {
+		return value;
+	}
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	
+
+}
