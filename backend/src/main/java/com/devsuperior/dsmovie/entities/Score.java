726071c07882fec76d47523f5ef6ddff497f43f0
Leonardo Eduardo <leonardo.eduardo@gmail.com>
2022-01-13 19:14:41 -0300

    Find movies

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -13,7 +13,7 @@ public class Score implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private ScorePk id;
+	private ScorePk id = new ScorePk();
 	
 	private Double value;
 	
